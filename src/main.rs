8d8c44f2053c0a1b5d159b33ccfa5873d906abda
Håkon <hakon.su@gmail.com>
2024-11-19 13:05:52 +0100

    Write function for fetching random word

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use reqwest::{self};
+use serde::{Deserialize, Serialize};
+use std::env;
+use dotenv::dotenv;
+
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Word {
+    _id: i32,
+    word: String,
+}
+
+fn get_word(api_key: &str) -> Result<Word, Box<dyn std::error::Error>> {
+    let url = format!("http://localhost:1000/Random?api_key={}", api_key);
+    let response = reqwest::blocking::get(&url)?;
+
+    let word = response.json::<Word>()?;
+    Ok(word)
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let api_key = env::var("API_KEY").expect("API_KEY not set as environment variable");
+
+    match get_word(&api_key) {
+        Ok(words) => {
+            println!("Received word: {:?}", words);
+        }
+        Err(e) => {
+            eprintln!("Error fetching word: {}", e);
+        }
+    }
 }
